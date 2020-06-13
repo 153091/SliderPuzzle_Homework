c9f237e974089309f8f2ca5d8be42a2bcde0c5c9
153091 <xpoint06@gmail.com>
2020-06-13 14:07:52 +0300

    83/100

---
+++ src/Board.java
@@ -0,0 +1,139 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+public class Board {
+
+    private int[] board;
+    private int size;
+    private int sizeSquare;
+
+    // create a board from an n-by-n array of tiles,
+    // where tiles[row][col] = tile at (row, col)
+    public Board(int[][] tiles) {
+        size = tiles.length;
+        sizeSquare = size*size;
+        board = new int[sizeSquare];
+        for(int i = 0 ; i < size; i++)
+            for (int j = 0; j < size; j++)
+                board[i*size + j] = tiles[i][j];
+    }
+
+    //?????
+    // string representation of this board
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(size + "\n");
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                s.append(String.format("%2d ", board[i*size + j]));
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
+
+    // board dimension n
+    public int dimension() {
+        return size;
+    }
+
+    // number of tiles out of place
+    public int hamming() {
+        int score = 0;
+        for (int i = 0; i < sizeSquare; i++) if (board[i] != i + 1 && board[i] != 0) score++;
+        //для n=3 максимум 8 очков, !! а не 9!!, поэтому если боард[i]=0 - не в счет
+        return score;
+    }
+
+    // sum of Manhattan distances between tiles and goal
+    public int manhattan() {
+        int score = 0;
+        for (int i = 0 ; i < sizeSquare; i++)
+            //если боард[i]=0 - не в счет
+            if (board[i] != 0)
+                score += Math.abs((board[i] - 1)/size - i/size) + Math.abs((board[i] - 1)%size - i%size);
+            return score;
+    }
+
+    // is this board the goal board?
+    public boolean isGoal() {
+        return hamming() == 0;
+    }
+
+    //???
+    // does this board equal y?
+    public boolean equals(Object y) {
+        Board that = (Board) y;
+        return Arrays.equals(this.board, that.board);
+    }
+
+    // all neighboring boards
+    // создаст стек с соседями Board
+    public Iterable<Board> neighbors() {
+        Stack<Board> sosedi = new Stack<Board>();
+        int zeroIndex = findZero();
+
+        int[] copy = board.clone(); // нужно ли это????
+
+        int i = zeroIndex/size;
+        int j = zeroIndex % size;
+        if (valid(i, j + 1)) sosedi.push(makeBoard(swap(copy, zeroIndex, zeroIndex + 1)));
+        if (valid(i, j - 1)) sosedi.push(makeBoard(swap(copy, zeroIndex, zeroIndex - 1)));
+        if (valid(i, j + 1)) sosedi.push(makeBoard(swap(copy, zeroIndex, zeroIndex + size)));
+        if (valid(i, j + 1)) sosedi.push(makeBoard(swap(copy, zeroIndex, zeroIndex - size)));
+        return sosedi;
+    }
+
+
+
+    // a board that is obtained by exchanging any pair of tiles
+    public Board twin() {
+        int[][] blocks = new int[size][size];
+        int index = 0;
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++) {
+                blocks[i][j] = board[i * size + j];
+                if (board[i*size + j] ==  0) index = i*size + j;
+            }
+        int i = 0;
+            //свап будет между 0-1 или 3-4  [3x3]
+            while (i == index || i + 1 == index || (i + 1)/size - i/size > 0) i++;
+
+            int swap = blocks[i/size][i%size]; //(i/size) = i; (i%size) = j
+            blocks[i/size][i%size] = blocks[i/size][i%size + 1];
+            blocks[i/size][i%size + 1] = swap;
+        return new Board(blocks);
+    }
+
+    private int findZero() {
+        for (int i = 0 ; i < sizeSquare; i++)
+            if (board[i] == 0) return i;
+            return -1;
+    }
+
+    private boolean valid(int x, int y) {
+        return (x >= 0 && y >= 0 && x < size && y < size);
+    }
+
+    // вернет новый массив с exch индексами
+    private int[] swap(int[] orig, int index1, int index2) {
+        int[] newBoard = orig.clone();
+        int t = newBoard[index1];
+        newBoard[index1] = newBoard[index2];
+        newBoard[index2] = t;
+        return newBoard;
+    }
+
+    // создаст новый элемент Board из массива
+    private Board makeBoard(int[] array){
+        int[][] newBoard = new int[size][size];
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                newBoard[i][j] = array[i*size + j];
+            return new Board(newBoard);
+    }
+
+    // unit testing (not graded)
+    public static void main(String[] args) {}
+
+}
---
+++ src/Solver.java
@@ -0,0 +1,152 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Solver {
+
+    private Node start;  // начальный
+    private Node last;  // победный шаг
+    private Node badTwin; // если решается он, то оригинал не решится
+    private int numOfMove;
+    private boolean solvable;
+    private MinPQ<Node> openQueue;  // used for the original Board
+    private MinPQ<Node> twinQueue; // used for the twin Board
+    private Stack<Board> step; // сюда записывать все шаги от победного к началу
+
+    //задаем Ноде
+    private class Node implements Comparable<Node> {
+        int step = 0;
+        Board board;
+        Node previous;
+        int fScore;
+
+        //  нужно уточнить , что и как входит в Ноде, раньше ты это делал в функции  Add, push, enqueue
+        public Node(Board b, int stepMade, Node prevNode) {
+            step = stepMade;
+            board = b;
+            previous = prevNode;
+            fScore = step + b.manhattan();
+        }
+
+        //сравнение
+        public int compareTo(Node other) {
+            return Integer.compare(fScore, other.fScore);
+        }
+    }
+
+
+
+
+
+    // find a solution to the initial board (using the A* algorithm)
+    public Solver(Board initial) {
+
+        openQueue = new MinPQ<Node>();
+        twinQueue = new MinPQ<Node>();
+        solvable = false; //изначально так
+        numOfMove = -1; // -1 т.к. 4 борда являются 3-мя шагами, т.к. первый не в счет
+        start = new Node(initial, 0, null); // задать стартовый Борд
+        badTwin = new Node(start.board.twin(), 0 , null); // плохой близнец для стартового Борда
+        openQueue.insert(start); // очередь для оригинала
+        twinQueue.insert(badTwin); // очередь для близнецов
+
+        Node current;
+        Node newNeighbor;
+        Node alternateCurrent; // для twin
+
+        while (!openQueue.isEmpty() || !twinQueue.isEmpty()) { // пока есть что доставать
+            current = openQueue.delMin(); // достаем с минимальным манхеттеном
+            alternateCurrent = twinQueue.delMin(); // достаем с минимальным манхеттеном
+
+            if (current.board.isGoal()) {
+                solvable = true;
+                last = current;
+                break;
+            }
+
+            if (alternateCurrent.board.isGoal())  // если решается он(близнец), то оригинал не решится
+                break;                            // solvable осталась false
+
+            // Для досок b принадлежащих стеку "соседи"
+            for (Board b : current.board.neighbors()) {
+
+                // ни шагу назад. Если сосед плиты"А" равен плите, что была до А. то пропускать тело цикла
+                if (current.previous != null && b.equals(current.previous.board)) continue;
+
+                newNeighbor = new Node(b, current.step + 1, current);
+                openQueue.insert(newNeighbor); //вставляем соседей
+            }
+
+            for (Board b : alternateCurrent.board.neighbors()) {
+
+                // ни шагу назад. Если сосед плиты"А" равен плите, что была до А. то пропускать тело цикла
+                if (alternateCurrent.previous != null && b.equals(alternateCurrent.previous.board)) continue;
+
+                newNeighbor = new Node(b, alternateCurrent.step + 1, alternateCurrent);
+                twinQueue.insert(newNeighbor); //вставляем соседей
+            }
+        }
+
+        //  когда нашелся победный (last)
+        if (solvable) {
+            step = new Stack<Board>();  // шаги в обратном порядке
+            Node cur = last;
+            while (cur != null) {
+                numOfMove++;
+                step.push(cur.board);
+                cur = cur.previous;
+            }
+        }
+
+
+
+    }
+
+
+
+    // is the initial board solvable? (see below)
+    public boolean isSolvable() {
+        return solvable;
+    }
+
+    // min number of moves to solve initial board; -1 if unsolvable
+    public int moves() {
+        return numOfMove; // изначально -1
+    }
+
+    // sequence of boards in a shortest solution; null if unsolvable
+    public Iterable<Board> solution() {
+        if (!isSolvable()) return null;
+        return step;
+    }
+
+
+
+
+    // test client (see below)
+    public static void main(String[] args) {
+
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] tiles = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                tiles[i][j] = in.readInt();
+        Board initial = new Board(tiles);
+
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+
+        // print solution to standard output
+        if (!solver.isSolvable())
+            StdOut.println("No solution possible");
+        else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution())
+                StdOut.println(board);
+        }
+    }
+
+}
